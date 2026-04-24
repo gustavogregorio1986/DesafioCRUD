4c67e9bd64662e2d363f0851a9c5ef33b691ee91
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-04-24 10:07:17 -0300

    Desenvolvendo a classe genericRepository no projeto

---
+++ DesafioCRUD.Repository/Repository/GenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using DesafioCRUD.Data.Data;
+using DesafioCRUD.Repository.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioCRUD.Repository.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DesafioCRUDContexto _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(DesafioCRUDContexto context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
